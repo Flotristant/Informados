17c667bf34604ed0ce9d8e3242562d3ece50e04e
Florencia <flotristant@gmail.com>
2014-08-05 09:33:25 -0300

    corrección filtro, no permitía autenticar el login

--- prototipo/Informados/grails-app/conf/informados/SecutiryFIltersFilters.groovy
+++ prototipo/Informados/grails-app/conf/informados/SecutiryFIltersFilters.groovy
@@ -8,10 +8,11 @@ class SecutiryFIltersFilters {
 				if  ((actionName == null) || (actionName.equals('index') && controllerName.equals('main') || (actionName.equals('registro') ))) {
 					return true
 				}
-				if (!session.user && !actionName.equals('login')) {
+				if (!session.user && !(actionName.equals('login') || actionName.equals('authenticate'))) {
 					redirect(action: 'login', controller:'persona')
 					return false
 				}
+				return true
             }
             after = { Map model ->
 
